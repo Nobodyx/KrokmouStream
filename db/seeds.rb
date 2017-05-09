ab5af0cbf14d7a14b2a749346e3012b14e1ce18e
Louis Beaumont <louis.beaumont@ynov.com>
2017-05-09 16:47:39 +0200

    Added media seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Medium.destroy_all
+
+m1 = Medium.create!(Media_ID: 1,
+Title: "Brice de nice",
+Type: "FILM",
+Style: "Comédie",
+Season: 0,
+Director: "James Huth")
