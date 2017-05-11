3d52546b2d1b452f9fc0784f60cffa8e7ec611e6
Nobodyx <thomas.palazzetti@ynov.com>
2017-05-11 09:24:53 +0200

    Modification de plusieurs fichiers pour debloquer la fonction du javascript et des styles qui ne s'appliquait pas.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require turbolinks
+//= require jquery.ui.widget
+//= require jquery.iframe-transport
+//= require jquery.fileupload
+//= require cloudinary/jquery.cloudinary
+//= require attachinary
 //= require_tree .
+//= require bootstrap-sprockets
