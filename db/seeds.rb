1f0cf2f39e0ae4a8fe48cf3094c8a5bf6932d8b3
Nobodyx <thomas.palazzetti@ynov.com>
2017-05-11 10:51:53 +0200

    Ajout de nouveau seed pour media

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,35 @@ Style: "Comédie",
 Season: 0,
 Director: "James Huth",
 photo: open_photo("BriceDeNice.jpg"))
+
+m2 = Medium.create!(Media_ID: 2,
+Title: "L'Arme fatale",
+Type: "SERIE",
+Style: "Comédie, Drame",
+Season: 1,
+Director: "Matthew Miller",
+photo: open_photo("armefatale.jpg"))
+
+m3 = Medium.create!(Media_ID: 3,
+Title: "13 Reasons Why",
+Type: "SERIE",
+Style: "DRAME",
+Season: 2,
+Director: "Carl Franklin",
+photo: open_photo("13reasonwhy.jpg"))
+
+m4 = Medium.create!(Media_ID: 4,
+Title: "Prison Break",
+Type: "SERIE",
+Style: "Thriller",
+Season: 5,
+Director: "Nelson McCormick",
+photo: open_photo("prisonbreak.jpg"))
+
+m5 = Medium.create!(Media_ID: 5,
+Title: "One Piece",
+Type: "ANIME",
+Style: "Action, Aventure",
+Season: 5,
+Director: "Eiichiro Oda",
+photo: open_photo("onepiece.jpg"))
