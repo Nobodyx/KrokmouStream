a8ea665e5b1c8d561ca86c8def12105603773b98
Nobodyx <thomas.palazzetti@ynov.com>
2017-05-17 18:38:19 +0200

    Enfin fonctionnalité MArquer comme vu fait en quasi totalité, reste à la gerer !

--- app/controllers/member_media_controller.rb
+++ app/controllers/member_media_controller.rb
@@ -2,4 +2,19 @@ class MemberMediaController < ApplicationController
 	def index
 		@member_media = MemberMedia.all
 	end
+    
+    def new
+        @medium = Medium.find(params[:medium_id])
+        MemberMedia.create!(user_id: current_user.id,
+            Member_Media_ID: 1,
+            Media: @medium.Media_ID,
+            current_season: 0,
+            current_episode: 0,
+            Done: false)
+        #@new_member_media = MemberMedia.new
+        #@new_member_media.user_id = current_user.id
+        #@new_member_media.Media = @medium.id
+        
+        redirect_to member_media_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :episodes
-  resources :member_media
+  resources :member_media, only: [:index]
   resources :media, only: [:show] do
-      resources :seasons, only: [:index]      
-      resources :member_media, only: [:create]
+      resources :seasons, only: [:index]
+      resources :member_media, only: [:new]
   end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
