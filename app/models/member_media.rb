28a5ff3f432446b6ff0cb99eb507be2f1340cbb4
Nobodyx <thomas.palazzetti@ynov.com>
2017-05-17 15:16:07 +0200

    Retour a MemberMedia

--- app/models/member_media.rb
+++ app/models/member_media.rb
@@ -1,4 +1,4 @@
-class MemberMedium < ApplicationRecord
+class MemberMedia < ApplicationRecord
     belongs_to:user
     has_one:medium
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :episodes
   resources :member_media
   resources :media, only: [:show] do
-      resources :seasons, only: [:index]
+      resources :seasons, only: [:index]      
+      resources :member_media, only: [:create]
   end
-  resources :member_media
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     mount Attachinary::Engine => "/attachinary"
