5c68982f1e993a511697a0ee0af8a0c0a66eaae5
Aleqsd <aleqsd@gmail.com>
2017-05-20 10:14:28 +0200

    try format js

--- app/controllers/api_v1/episodes_controller.rb
+++ app/controllers/api_v1/episodes_controller.rb
@@ -1,6 +1,12 @@
 class ApiV1::EpisodesController < ActionController::Base
   def index
     @episodes = Episode.all
+
+    respond_to do |format|
+      format.js do
+        @episodes = Episode.all
+      end
+    end
   end
 
   def show
