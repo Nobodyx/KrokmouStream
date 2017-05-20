9e61333964b193b0ec2fb97672f4c3198832cae3
Aleqsd <aleqsd@gmail.com>
2017-05-20 11:52:44 +0200

    fix bug devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   acts_as_token_authenticatable
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_token
   has_many:member_media
   has_attachment :avatar, accept: [:jpg, :png, :jpeg]
   validates :username, presence: true, uniqueness: true
