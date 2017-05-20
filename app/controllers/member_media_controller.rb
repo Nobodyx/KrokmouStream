3da6256e428efb8fc775cb86ec4110570f204953
Nobodyx <thomas.palazzetti@ynov.com>
2017-05-20 14:22:35 +0200

    Mise en place de marquage d'espisode OK - Prochaine amelioration ajaxification

--- app/controllers/member_media_controller.rb
+++ app/controllers/member_media_controller.rb
@@ -20,8 +20,16 @@ class MemberMediaController < ApplicationController
         redirect_to member_media_path
     end
     
-    def update(id,season_number,episode_number)
-        MemberMedia.update(id, :current_season => season_number, :current_episode => episode_number)
-        redirect_to member_media_path
+    #def update(id,season_number,episode_number)
+        #MemberMedia.update(id, :current_season => season_number, :current_episode => episode_number)
+        #redirect_to member_media_path
+    #end
+    
+    def update
+        MemberMedia.update(member_medium_params)
+    end
+    
+    def member_medium_params
+        params.require(:member_media).permit(:id, :current_season, :current_episode)
     end
 end
