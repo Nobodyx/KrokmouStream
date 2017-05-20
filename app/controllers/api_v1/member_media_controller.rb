92fdbeecb6f3d42468f5e01dba956c81b8c8b404
Aleqsd <aleqsd@gmail.com>
2017-05-20 12:40:48 +0200

    change where userid

--- app/controllers/api_v1/member_media_controller.rb
+++ app/controllers/api_v1/member_media_controller.rb
@@ -2,6 +2,6 @@ class ApiV1::MemberMediaController < ApplicationController
   acts_as_token_authentication_handler_for User
 
   def index
-    @member_media = MemberMedia.find(:all, :user_id => current_user.id)
+    @member_media = MemberMedia.where(:user_id => current_user.id)
   end
 end
