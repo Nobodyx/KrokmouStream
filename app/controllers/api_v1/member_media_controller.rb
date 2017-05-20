d2e40f454e0b2aeb981241fe89a1f3ff7f46d89b
Aleqsd <aleqsd@gmail.com>
2017-05-20 12:56:57 +0200

    test condition userid 1

--- app/controllers/api_v1/member_media_controller.rb
+++ app/controllers/api_v1/member_media_controller.rb
@@ -2,6 +2,6 @@ class ApiV1::MemberMediaController < ApplicationController
   acts_as_token_authentication_handler_for User
 
   def index
-    @member_media = MemberMedia.where(:user_id => 1).to_a
+    @member_media = MemberMedia.where(user_id: 1)
   end
 end
