e30c173c8425abd7312afcee6f0c337eed543028
Aleqsd <aleqsd@gmail.com>
2017-05-20 11:35:31 +0200

    Change user model auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
   has_many:member_media
   has_attachment :avatar, accept: [:jpg, :png, :jpeg]
   validates :username, presence: true, uniqueness: true
